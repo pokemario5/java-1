8ff4632ac67564114248b18ccbeb6cdc45ac7f87
SkilStak Coding Arts <learn@skilstak.com>
2015-08-29 17:05:35 -0400

    save

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+
+import skilstak.c;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+      System.out.println(c.cl + c.y + "Hello" + c.b + " world!" + c.x);
+    }
+}
+
+
+
+
